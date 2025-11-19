e2a901eb652d4634d3211d85595b4f05fa8843b3
matheus lima <matheus19win@gmail.com>
2025-11-18 21:23:08 -0300

    teste de fluxo de checkout utilizando intercept nas interações com o carrinho

--- cypress/support/Sup.Checkout.js
+++ cypress/support/Sup.Checkout.js
@@ -1,17 +1,13 @@
 class realizarCheckout {
 
     adicionarItem() {
-        // exemplo para clicar em um produto
         cy.get('[href="/Tab/Browse"]').click();
         cy.get(':nth-child(15) > .r-18u37iz > :nth-child(2) > [data-testid="productDetails"]').click();
         cy.get('[data-testid="addToCart"]').click();
     }
 
     atualizarQuantidade(qtd) {
-        // botão de aumentar quantidade
         cy.get('[data-testid="addItem"] > .css-146c3p1').click().click();
-        // ou escrever direto no input
-        // cy.get('[data-testid="cart-qty"]').clear().type(qtd);
     }
 
     removerItem() {
