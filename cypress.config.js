97881301a7515e791261db76f4c5b4103876a3a5
matheus lima <matheus19win@gmail.com>
2026-04-14 22:20:30 -0300

    AppActions com pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "y8cgnm",
+
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "reports",
+    overwrite: false,
+    html: true,
+    json: false
+  },
+
   e2e: {
     baseUrl:'http://lojaebac.ebaconline.art.br/',
     experimentalStudio: true,
